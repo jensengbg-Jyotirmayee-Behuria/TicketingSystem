9dc795c0d351cd450debf2d77f1e815536cd1e21
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-06-11 07:37:20 +0200

    added some function

--- Database/database.js
+++ Database/database.js
@@ -120,42 +120,6 @@ function generateTicketNumber() {
     return finalTicketNumber;
 }
 
-exports.verifyTicket=(ticket) => 
-{
-    console.log("ticket to search:" +ticket);
-       
-
-    let ticketExist=ticketDb.get('Tickets')
-                        .find({ ticketId: ticket })
-                        .value();
-    console.log(ticketExist.id);
-         //exists but , is that alredy verfied?
-    if(ticketExist.verified==undefined)
-    {
-        let verifyMessage = {
-            verifyStatus : 'InvalidTicket', message : "Ticket is not valid"};
-            
-            return verifyMessage;
-    }
-
-    if(ticketExist.verified==true)
-    {        
-        console.log("its already verified")       
-
-        verifyMessage = {
-            verifyStatus : 'AlredyVerified', message : "This ticket is already verified by staff"};
-                 
-        console.log(verifyMessage);
-        return verifyMessage;
-    }
-
-    ticketDb.get('Tickets').find({ ticketId: ticket }).assign({ verified: true }).write();   
-    
-    return  verifyMessage = {
-        verifyStatus : 'Verified', message : "This ticket verified succesfully"};
-            
-    
-}
 function createNewTicketObjectFromEvent(event)
 {
     ticket = {
@@ -169,6 +133,44 @@ function createNewTicketObjectFromEvent(event)
     };
      return ticket;
 }
+
+exports.verifyTicket=(ticket) => 
+{
+    console.log("ticket to search:" +ticket);
+       
+
+    let ticketExist=ticketDb.get('Tickets')
+                        .find({ ticketId: ticket })
+                        .value();
+    
+         //exists but , is that alredy verfied?
+    if(ticketExist==undefined)
+    {
+        let verifyMessage = {
+            verifyStatus : 'InvalidTicket', message : "Ticket is not valid"};
+            
+            return verifyMessage;
+    }
+
+    if(ticketExist.verified==true)
+    {        
+        console.log("its already verified")       
+
+        verifyMessage = {
+            verifyStatus : 'AlredyVerified', message : "This ticket is already verified!"};
+                 
+        console.log(verifyMessage);
+        return verifyMessage;
+    }
+
+    ticketDb.get('Tickets').find({ ticketId: ticket }).assign({ verified: true }).write();   
+    
+    return  verifyMessage = {
+        verifyStatus : 'Verified', message : "This ticket verified succesfully"};
+            
+    
+}
+
 exports.addItemInTickets = (index ) => {
    // let itemtoAdd = parseInt(index);
     console.log('add Item In Tickets: ', index);     
@@ -189,44 +191,3 @@ exports.addItemInTickets = (index ) => {
 } 
 
  
-// exports.addItemInOrders = (searchTerm) => {
-//     let count = 0;
-//     let itemtoAdd = parseInt(searchTerm);
-//     console.log(itemtoAdd);
-
-//     /* DISPLAY: error message if you try to add a product that does not exist.*/
-//     const matchProduct = eventDb.get('Events').find({ id: itemtoAdd }).value();
-//     let ticketIndexToChange = eventDb.get('Events').filter({ id: itemtoAdd }).map('tickets').value();
-//     ticketIndexToChange = parseInt(ticketIndexToChange);
-//     ticketIndexToChange = ticketIndexToChange - 1;
-    
-//     console.log("ticket Index To Change: ", ticketIndexToChange);
-//     //console.log(matchProduct);
-
-//     if(matchProduct === undefined){
-//         alertMessage = {
-//             status : 'ERROR', message : "Invalid product ID. Enter correct ID. " };
-//             console.log(alertMessage);
-//     }else{
-//         alertMessage = {
-//             status : 'SUCCESS', message : "Valid ID. Item found in Events. "
-//         }
-//         console.log(alertMessage);
-//         count += 1;
-
-//     // /* NOT ADD THE SAME PRODUCT TO CART CHECK */     
-//     //     const checkInOrders = findItemInOrders(itemtoAdd);//find item in orders
-    
-  
-//         eventDb.get('Events').find({ id: itemtoAdd }).assign({ tickets: ticketIndexToChange }).write(); //change the ticket number when adding a ticket in orders
-//         console.log("The ticket number now: ", matchProduct.tickets);
-//         orderDb.get('Orders').push(matchProduct).write();
-        
-//         alertMessage = {
-//             status : 'SUCCESS', message : "Item added in the Orders"};
-//             console.log(alertMessage);
-//         }
-  
-    
-//     return alertMessage;
-// } 
